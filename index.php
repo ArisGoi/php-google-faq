211915f41c9574e11c9e3b1c2c7dd0a443255f17
ArisGoi <aris.goi97@gmail.com>
2021-10-20 14:19:05 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Google FAQS</title>
+
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500&display=swap" rel="stylesheet">
+    <!-- /Google Fonts -->
+
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>HEADER</header>
+
+    <main></main>
+
+    <footer>Footer</footer>
+</body>
+</html>
